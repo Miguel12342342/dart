3c8430b85bba5b4dd7574567cfb15109f58fc7bd
Miguel Angelo <63747769+Miguel12342342@users.noreply.github.com>
2026-04-02 14:51:55 -0300

    study: variaveis - var, const, final, late e nullable

---
+++ lib/tipos/variaveis.dart
@@ -0,0 +1,43 @@
+//O que sao variaveis?
+
+/*
+
+Sao espaço em memoria que voce aloca para voce colocar ali uma informação
+durante o tempo de execução do nosso sistema, ou seja, durante 
+o tempo que o programa esta rodando, voce pode colocar uma
+informação ali e depois pegar essa informação para usar
+em algum lugar do seu código.
+
+Quando voce fala em variaveis, voce subsentende que pode mudar
+o valor dela, o nome ja diz, variavel, ou seja, ela pode variar
+, ela pode mudar...
+
+*/
+
+void main(){
+
+var nome = "Miguel";
+nome = "Miguel Angelo";
+print(nome);
+
+var idade = 24;
+print(idade);
+
+var peso = 88.5;
+print(peso);
+
+var maiorIdade = true;
+print(maiorIdade);
+
+const nomeCompleto = "Miguel Angelo Lima Antunes";
+print(nomeCompleto);
+
+final int? altura;
+altura = 80;
+print(altura);
+
+late String nomePai;
+nomePai = 'Jose';
+print(nomePai);
+
+}
